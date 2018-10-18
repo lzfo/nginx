51229a44bbfc2defc5a5714fc11fd799c9f06f10
Zolf <lzf@kiwik.cn>
2018-10-18 16:33:48 +0800

    feat: bump nginx version to 1.15.5

--- build_nginx.sh
+++ build_nginx.sh
@@ -8,10 +8,10 @@
 set -e
 
 # names of latest versions of each package
-export NGINX_VERSION=1.14.0
+export NGINX_VERSION=1.15.5
 export VERSION_PCRE=pcre-8.42
 export VERSION_ZLIB=zlib-1.2.11
-export VERSION_OPENSSL=openssl-1.0.2o
+export VERSION_OPENSSL=openssl-1.0.2p
 export VERSION_NGINX=nginx-$NGINX_VERSION
 # URLs to the source directories
 export SOURCE_PCRE=https://ftp.pcre.org/pub/pcre/
